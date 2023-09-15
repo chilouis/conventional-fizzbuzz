429b1eab9ad0cf6f652d405cd83d701a0bc49383
chilouis <louiscwlee@gmail.com>
2023-09-15 14:57:54 +0900

    feat: Print fizz
    Print fizz if i is multiple of 3
    Print buzz if i is multiple of 5
    Print fizzbuzz if i is multiple of 15
    Otherwise, print i itself.

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+def do_fizzbuzz():
+    '''
+    fizzbuzz 기능 수행
+    정해진 숫자에 대해
+    3의 배수는 fizz
+    5의 배수는 buzz
+    15의 배수는 fizzbuzz
+    나머지는 숫자 그대로 출력
+    '''
+    for i in range(1,20+1):
+        if i%15 == 0:
+            print('fizzbuzz')
+        elif i%3 == 0:
+            print('fizz')
+        elif i%5 == 0:
+            print('buzz')
+        else:
+            print(i)
+
+if __name__=='__main__':
+    do_fizzbuzz()
